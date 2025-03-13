0b5ed316d2ce0ecd05973e4b0b53d5edf12e72c7
Ayush Karan <ayushkaran328@gmail.com>
2025-03-14 00:24:16 +0530

    Update App.jsx
    added ParthHome.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./login";
 import Home from "./home";
+import Parth from "./PartHome"
 
 function App() {
     return (
@@ -12,8 +13,12 @@ function App() {
                 {/* Protected Routes */}
                 <Route path="/home" element={<Home />} />
 
+                <Route path="/PartHome" element={<Parth />} />
+
                 {/* Redirect unknown routes to login */}
                 <Route path="*" element={<Navigate to="/login" />} />
+
+
             </Routes>
         </Router>
     );
