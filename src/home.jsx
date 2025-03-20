a981c495be08b08b3694126dd4df5a1d016b658a
parth <your-email@example.com>
2025-03-20 17:05:10 +0530

    buttons are added

--- src/home.jsx
+++ src/home.jsx
@@ -43,7 +43,7 @@ const Home = () => {
     // Function to open Google Drive link
     const openDrive = () => {
         window.location.href="https://drive.google.com/drive/folders/17Gw-3AjOVSjPvzst0PvoNj94zAoJVVy0?usp=drive_link";
-    }
+    };
 
     return (
         <div className="home-container">
@@ -75,7 +75,8 @@ const Home = () => {
 
             {/* Buttons */}
             <div className="button-container">
-                <button className="custom-button">View Payment</button>
+                <button className="custom-button">Settlement</button>
+                <button className="custom-button">Payment History</button>
                 <button className="custom-button" onClick={openDrive}>Photo Gallery</button>
             </div>
         </div>
