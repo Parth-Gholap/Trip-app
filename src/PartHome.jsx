8cbdc0f48cef2bf4ca64f67d9431bb7d5f508116
parth <your-email@example.com>
2025-03-20 23:32:53 +0530

    corrected css files name

--- src/PartHome.jsx
+++ src/PartHome.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./Home.css"; // Import Home.css for consistent styling
+import "./home.css"; // Import Home.css for consistent styling
 import { useNavigate } from "react-router-dom";
 
 const ParthHome = () => {
